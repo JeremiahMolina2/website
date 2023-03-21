d8f4961931d932ffb759670c7c91d0508e4242a0
JeremiahMolina2 <126709228+JeremiahMolina2@users.noreply.github.com>
2023-03-21 17:04:20 +0000

    Tue, Mar 21, 2023, 10:04 AM -07:00

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,10 @@
 const counter = document.querySelectorAll('.counter')
+
+counter.forEach(counter => {
+    counter.innerText= '0'
+
+    const updateCounter = () => {
+        const target = counter.getAttribute('data-target')
+        console.log(target)
+    }
+})
