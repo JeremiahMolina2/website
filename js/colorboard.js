d049fc5c2889f1a9bb5060f72d707d658cdb7e91
JeremiahMolina2 <126709228+JeremiahMolina2@users.noreply.github.com>
2023-03-22 16:36:02 +0000

    Wed, Mar 22, 2023, 9:36 AM -07:00

---
+++ js/colorboard.js
@@ -0,0 +1,19 @@
+const container = document.getElementById('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+cont SQUARES = 500
+
+for(let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+    container.appendChild(square)
+    square.addEventListener('mouseover', () => setColor(square))
+}
+
+
+function setColor(element){
+    const color = getRandomColor()
+}
+
+funstion getRandomColor() {
+    return colors[Math.floor(Math.random())]
+}
