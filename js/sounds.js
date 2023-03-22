d6e08f4119cc02d641e25f4bc86ab3d8cf9b4b49
JeremiahMolina2 <126709228+JeremiahMolina2@users.noreply.github.com>
2023-03-22 17:03:25 +0000

    Wed, Mar 22, 2023, 10:03 AM -07:00

--- js/sounds.js
+++ js/sounds.js
@@ -1,12 +1,12 @@
 const sounds =['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
 
 sounds.forEach(sound => {
-    const btn = document.create.Element('button')
+    const btn = document.createElement('button')
     btn.classList.add('.btn')
     btn.innerText = sound
     btn.addEventListener('click', () => {
         stopSongs()
-        document.getElementById(sound).play()
+        document.getElementById(sound).play(
     })
     document.getElementById('buttons').appendChild(btn)
 })
