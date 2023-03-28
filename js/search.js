81fe3f770bfc3621be65e95aa2df97e626dc6170
JeremiahMolina2 <126709228+JeremiahMolina2@users.noreply.github.com>
2023-03-28 17:22:40 +0000

    Tue, Mar 28, 2023, 10:22 AM -07:00

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
 console.log("HELLO")
 
 const search = document.querySelector('.search')
-const btn document.querySelector('btn')
+const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
 
 btn.addEventListener('click', () => {
